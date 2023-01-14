d8e03f136f54fa6a325cd64fe43830730ff9157d
mkizka <g.mkizka@gmail.com>
2023-01-14 12:41:54 +0900

    自分専用に

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ import pin from "./api/pin.js";
 import topLangs from "./api/top-langs.js";
 import wakatime from "./api/wakatime.js";
 const app = express();
-app.get("/api", api);
-app.get("/api/pin", pin);
-app.get("/api/top-langs", topLangs);
-app.get("/api/wakatime", wakatime);
+function overwriteQuery(handler) {
+  return (req, res) => {
+    req.query.username = "mkizka";
+    return handler(req, res);
+  };
+}
+app.get("/api", overwriteQuery(api));
+app.get("/api/pin", overwriteQuery(pin));
+app.get("/api/top-langs", overwriteQuery(topLangs));
+app.get("/api/wakatime", overwriteQuery(wakatime));
 app.listen(3000);
